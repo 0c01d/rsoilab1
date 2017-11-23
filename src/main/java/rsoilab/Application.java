43dc8b9358093dd240f5b724e3787f79adb90a45
0co1d <artem-posmo2012@mail.ru>
2017-11-23 11:47:19 +0300

    change Application.class for .jar console run

--- src/main/java/rsoilab/Application.java
+++ src/main/java/rsoilab/Application.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+/*@Configuration*/
+/*@EnableAutoConfiguration*/
+/*@ComponentScan*/
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
